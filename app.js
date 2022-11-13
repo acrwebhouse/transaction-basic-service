8acacb7008e22db85a10480bae96d4627093d356
chengyen.chiang <chengyen.chiang@leediancn.com>
2022-11-14 00:13:45 +0800

    framwork ok

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ const mongoDBPort = config.mongoDBPort;
 const mongoDBName = config.mongoDBName;
 const server = require('http').createServer(app);
 const serverUse=require('./lib/serverUse');
-const userRestApi = require("./lib/rest_api/user");
+const transactionRestApi = require("./lib/rest_api/transaction");
 console.log(config)
 dataBaseInit.mongoDBInit(mongoDBPort,mongoDBName);
 serverUse.on(app);
-userRestApi.on(app);
+transactionRestApi.on(app);
 server.listen(process.env.PORT||serverPort);
 console.log("現在使用" + serverPort + "port");
